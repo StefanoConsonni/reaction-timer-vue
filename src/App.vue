1578fc67d79e2e658d04c72089479b7a0ad2a72c
Stefano Consonni <stefanoconsonni@ymail.com>
2023-07-28 15:42:18 +0200

    create Results.vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <h1>Reaction Timer</h1>
+  <p>Click on the Play button and then click on the green box as soon as it appears!</p>
   <button @click="start" :disabled="isPlaying">Play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame" />
-  <p v-if="showResults">Reaction time: {{ score }} ms</p>
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
   import Block from "./components/Block.vue";
+  import Results from "./components/Results.vue";
 
   export default {
     name: "App",
-    components: { Block },
+    components: { Block, Results },
     data() {
       return {
         delay: null,
