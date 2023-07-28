d890a751a84e0397fca9a8792b80ad8fb5b52845
Stefano Consonni <stefanoconsonni@ymail.com>
2023-07-28 15:30:05 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,46 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <p v-if="showResults">Reaction time: {{ score }} ms</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Block from "./components/Block.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "App",
+    components: { Block },
+    data() {
+      return {
+        delay: null,
+        isPlaying: false,
+        score: null,
+        showResults: false,
+      };
+    },
+    methods: {
+      start() {
+        this.delay = 2000 + Math.random() * 5000;
+        this.isPlaying = true;
+        this.showResults = false;
+      },
+      endGame(reactionTime) {
+        this.score = reactionTime;
+        this.isPlaying = false;
+        this.showResults = true;
+      },
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+    margin-top: 60px;
+  }
 </style>
